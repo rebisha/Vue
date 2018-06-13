0785a6bffcbc54964fa106410c5e31dd26a087eb
AP-MEDIA\rebisha.pokharel <rebisha.pokharel@isobar.com>
2018-06-13 18:25:11 +1000

    vue project

---
+++ Vue-project/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+<v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :mini-variant="mini"
+      light
+      absolute
+    >
+  <v-app>
+    <v-toolbar class="deep-purple">
+      <v-toolbar-title> Vuetify Tutorial </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn flat dark>Vue </v-btn>
+        <v-btn flat dark>VueX </v-btn>
+        <v-btn flat dark>VueXL </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
